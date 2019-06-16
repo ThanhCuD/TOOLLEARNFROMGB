c79574c7bf6d1a98f78669e6f3f4e4178f18b837
nguyen thanh <nguyenthanh209415@gmail.com>
2019-06-16 21:10:09 +0700

    Commit tool

---
+++ TOOL/TOOL/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TOOL
+{
+    partial class TOOL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbXml = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNameOfClass = new System.Windows.Forms.TextBox();
+            this.btGenXML = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtbXml
+            // 
+            this.rtbXml.Location = new System.Drawing.Point(724, 58);
+            this.rtbXml.Name = "rtbXml";
+            this.rtbXml.Size = new System.Drawing.Size(513, 290);
+            this.rtbXml.TabIndex = 9;
+            this.rtbXml.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Name Of Class";
+            // 
+            // txtNameOfClass
+            // 
+            this.txtNameOfClass.Location = new System.Drawing.Point(105, 23);
+            this.txtNameOfClass.Name = "txtNameOfClass";
+            this.txtNameOfClass.Size = new System.Drawing.Size(283, 20);
+            this.txtNameOfClass.TabIndex = 7;
+            // 
+            // btGenXML
+            // 
+            this.btGenXML.Location = new System.Drawing.Point(724, 29);
+            this.btGenXML.Name = "btGenXML";
+            this.btGenXML.Size = new System.Drawing.Size(75, 23);
+            this.btGenXML.TabIndex = 6;
+            this.btGenXML.Text = "GenXML";
+            this.btGenXML.UseVisualStyleBackColor = true;
+            this.btGenXML.Click += new System.EventHandler(this.btGenXML_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(678, 290);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // TOOL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 358);
+            this.Controls.Add(this.rtbXml);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNameOfClass);
+            this.Controls.Add(this.btGenXML);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "TOOL";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbXml;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNameOfClass;
+        private System.Windows.Forms.Button btGenXML;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TOOL/TOOL/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace TOOL
+{
+    public partial class TOOL : Form
+    {
+        /// <summary>
+        /// Consts
+        /// </summary>
+        private class Consts
+        {
+            /// <summary>
+            /// TabAndNewLine
+            /// </summary>
+            public const string TabAndNewLine = "\t\n";
+
+            /// <summary>
+            /// Tab
+            /// </summary>
+            public const string Tab = "\t";
+
+            /// <summary>
+            /// Root name of file XML
+            /// </summary>
+            public const string Root = "Root";
+
+            /// <summary>
+            /// NameOfXML
+            /// </summary>
+            public const string NameOfXML = "NameOfXML";
+
+            /// <summary>
+            /// NullLower
+            /// </summary>
+            public const string NullLower = "null";
+
+        }
+
+        public TOOL()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void btGenXML_Click(object sender, EventArgs e)
+        {
+            GenXML();
+        }
+
+        #region Method
+        /// <summary>
+        /// Gen XML from excel
+        /// </summary>
+        private void GenXML()
+        {
+            try
+            {
+                // read from RictTextBox
+                string text = richTextBox1.Text;
+                // push every element in to a Array
+                var arrDoc = Regex.Split(text, Consts.TabAndNewLine);
+                // take values of all
+                var arrNote = arrDoc.Take(arrDoc.Length - 1).Skip(1).Select(x => Regex.Split(x, Consts.Tab)).ToArray();
+                // Take name of element
+                var nameElement = Regex.Split(arrDoc[0], Consts.Tab);
+
+                XmlDocument doc = new XmlDocument();
+                // Create a root
+                XElement xml = new XElement(Consts.Root);
+
+                // loop them arrNote
+                for (int i = 0; i < arrNote.Length; i++)
+                {
+                    // check Null or empty 
+                    var nameOfXMl = !string.IsNullOrEmpty(txtNameOfClass.Text.Trim()) ? txtNameOfClass.Text.Trim() : Consts.NameOfXML;
+
+                    // create elements
+                    XElement elements = new XElement(nameOfXMl);
+                    for (int j = 0; j < nameElement.Length; j++)
+                    {
+                        // Check null for value elment
+                        if (arrNote[i][j].ToLower() != Consts.NullLower)
+                        {
+                            XElement element = new XElement(nameElement[j], arrNote[i][j]);
+                            elements.Add(element);
+                        }
+                    }
+                    // Add them
+                    xml.Add(elements);
+                }
+                // show xml 
+                rtbXml.Text = xml.ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(String.Format("Error :{0}", ex));
+            }
+        }
+        #endregion
+
+
+       
+    }
+}
